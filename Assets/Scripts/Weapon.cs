3f0f34963a5e169ac6fa8b92f51182d51c1eeb97
Martin Garnier <m.garnier@student.isartdigital.com>
2020-07-25 16:49:53 +0200

    [fix] fire Rate

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,25 +7,27 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     Bullet AmmoType;
 
-    bool fired = false;
+    float Lastfired;
+
+    [SerializeField]
+    float fireRate = 1.0f;
+
+    [SerializeField]
+    IsometricPlayerMovementController IsoController;
+
     void Shoot()
     {
-        
-        GameObject Go = Instantiate(AmmoType.gameObject, FindObjectOfType<IsometricPlayerMovementController>().transform.position + 0.5f * new Vector3(IsometricPlayerMovementController.lastWantedDirection.normalized.x, IsometricPlayerMovementController.lastWantedDirection.normalized.y), Quaternion.identity);
+        GameObject Go = Instantiate(AmmoType.gameObject, IsoController.transform.position + 0.5f * new Vector3(IsometricPlayerMovementController.lastWantedDirection.normalized.x, IsometricPlayerMovementController.lastWantedDirection.normalized.y), Quaternion.identity);
         Go.GetComponent<Bullet>().SetRadius(AmmoType.script.Radius);
         Go.GetComponent<Rigidbody2D>().AddForce(IsometricPlayerMovementController.lastWantedDirection.normalized * AmmoType.script.Speed, ForceMode2D.Impulse);
     }
 
     private void Update()
     {
-        if(Input.GetAxis("Fire1") == 1 && !fired)
+        if(Input.GetAxis("Fire1") == 1 && Time.time > Lastfired)
         {
-            fired = true;
+            Lastfired = fireRate + Time.time;
             Shoot();
-        }
-        else if(Input.GetAxis("Fire1") == 0)
-        {
-            fired = false;
         }
     }
 }
