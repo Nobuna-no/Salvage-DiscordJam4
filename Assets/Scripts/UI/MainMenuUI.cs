b2c68fc14efdd9708ef86dfd30b02423d727c457
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2020-07-25 11:36:27 +0200

    Linked quit button

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+	[SerializeField] private GameObject quitButton = null;
+
+#if UNITY_WEBGL
+	private void Awake()
+	{
+		quitButton.SetActive(false);
+	}
+#endif
+
+	public void StartGame()
+	{
+		// TODO: Load scene 1
+	}
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+        Application.Quit ();
+#endif
+	}
+}
