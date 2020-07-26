fafc94c407f6be05c65840b08d36da4244210e92
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2020-07-26 13:17:07 +0200

    Fixed pool with current map

--- Assets/Scripts/AI/EnemyPool.cs
+++ Assets/Scripts/AI/EnemyPool.cs
@@ -3,14 +3,16 @@
 public class EnemyPool : Pool
 {
 	[SerializeField] private int initialCount;
-	[SerializeField] private CircleCollider2D[] spawnPoints;
+	[SerializeField] private Transform spawnPointsFolder;
 
 	private void Awake()
 	{
+		PoolSpawnRadius[] spawnPoints = spawnPointsFolder.GetComponentsInChildren<PoolSpawnRadius>();
+
 		for (int i = 0; i < initialCount; ++i)
 		{
-			CircleCollider2D spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-			Vector2 circlePos = Random.insideUnitCircle * spawnPoint.radius;
+			PoolSpawnRadius spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+			Vector2 circlePos = Random.insideUnitCircle * spawnPoint.Radius;
 			Vector3 spawnPos = new Vector3(circlePos.x, circlePos.y, 0.0f) + spawnPoint.transform.position;
 			AddObject(Random.Range(0, m_prefabs.Length), spawnPos);
 		}
