e1df46a948266244b49cf94d641b1120b3beaf35
Martin Garnier <m.garnier@student.isartdigital.com>
2020-07-26 20:31:56 +0200

    [add] controller shake

--- Assets/Scripts/Ref/BasicCameraFollow.cs
+++ Assets/Scripts/Ref/BasicCameraFollow.cs
@@ -1,30 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Cinemachine;
+using XInputDotNetPure;
+
 
 public class BasicCameraFollow : MonoBehaviour 
 {
     [SerializeField]
     CinemachineVirtualCamera cam;
 
-    float AmplitudeGain = 0.5f;
-    float FrequencyGain = 0.5f;
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    bool shake = false;
+
+    [SerializeField]
+    EnemyPool pool;
 
     private void Start()
     {
         EndShake();
+        pool.OnAllEnemyDisabled.AddListener(EndShake);
     }
 
-    public void StartShake()
+    void Update()
     {
-        cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = AmplitudeGain;
-        cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = FrequencyGain;
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex) i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+    }
+
+    public void StartShake(CameraShakeSO Intensity)
+    {
+        StopAllCoroutines();
+        EndShake();
+
+        cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = Intensity.AmplitudeGain;
+        cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = Intensity.FrequencyGain;
+        GamePad.SetVibration(playerIndex, Intensity.ControllerIntensity.x, Intensity.ControllerIntensity.y);
+        if (Intensity.Duration > 0.0f)
+        {
+            StartCoroutine(WaitEndShake(Intensity.Duration));
+        }
     }
 
     public void EndShake()
     {
         cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0.0f;
         cam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 0.0f;
+        GamePad.SetVibration(playerIndex, 0.0f, 0.0f);
+    }
+
+    IEnumerator WaitEndShake(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        EndShake();
     }
 
 }
