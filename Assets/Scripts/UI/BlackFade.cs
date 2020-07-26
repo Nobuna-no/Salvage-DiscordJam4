592aa8150c6a84f6d4bac367869e74e3325c0037
Jannah MEKHAEMAR <j.mekhaemar@student.isartdigital.com>
2020-07-26 16:46:04 +0200

    Added black fade screen

---
+++ Assets/Scripts/UI/BlackFade.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BlackFade : MonoBehaviour
+{
+	[SerializeField] private bool startBlack = true;
+	[SerializeField] private float speed = 1.0f;
+	private Image image = null;
+
+	public UnityEvent OnFadeOver = new UnityEvent();
+
+	private float Alpha
+	{
+		get => image.color.a;
+		set
+		{
+			Color c = image.color;
+			c.a = value;
+			image.color = c;
+		}
+	}
+
+	private void Awake()
+	{
+		image = GetComponent<Image>();
+		Alpha = startBlack ? 1.0f : 0.0f;
+	}
+
+	public Coroutine StartFading(bool fadeToBlack)
+	{
+		StopAllCoroutines();
+		return StartCoroutine(fadeToBlack ? FadeIn() : FadeOut());
+	}
+
+	private IEnumerator FadeIn()
+	{
+		while (Alpha < 1.0f)
+		{
+			Alpha = Mathf.Min(1.0f, Alpha + speed * Time.deltaTime);
+			yield return null;
+		}
+		OnFadeOver.Invoke();
+	}
+
+	private IEnumerator FadeOut()
+	{
+		while (Alpha > 0.0f)
+		{
+			Alpha = Mathf.Max(0.0f, Alpha - speed * Time.deltaTime);
+			yield return null;
+		}
+		OnFadeOver.Invoke();
+	}
+}
