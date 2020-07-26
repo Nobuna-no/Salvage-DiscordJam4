86658778e2e977b5cbe1d311901f9a0f69c5dcdf
Martin Garnier <m.garnier@student.isartdigital.com>
2020-07-26 12:42:39 +0200

    [add] sound

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> Sounds;
+
+    [SerializeField]
+    AudioSource Source;
+
+    public void PlayMusic()
+    {
+        Source?.PlayOneShot(Sounds[Random.Range(0, Sounds.Count)]);
+    }
+
+    public void PlayOneMusic()
+    {
+        Source?.PlayOneShot(Sounds[0]);
+    }
+}
