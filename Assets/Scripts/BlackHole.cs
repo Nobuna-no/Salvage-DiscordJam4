e0e3e13124688a8be553e8a26c15d2a4540e00f4
Martin Garnier <m.garnier@student.isartdigital.com>
2020-07-26 15:53:36 +0200

    [polish] Vacuum

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    [SerializeField]
+    float AttractorMultiplicator = 100.0f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Vector3 finalPos = transform.position - collision.transform.position;
+            finalPos.z = 0;
+            finalPos = finalPos.normalized * AttractorMultiplicator;
+            collision.GetComponent<Rigidbody2D>().AddForce(finalPos, ForceMode2D.Force);
+        }
+    }
+}
